6b5f8f67ad1471b387a56f5e4f9658e3f0d9b904
gmile <iamexile@gmail.com>
2017-04-08 13:50:21 +0200

    WIP: generating input file from existing mysql db

--- generate_input_file.rb
+++ generate_input_file.rb
@@ -15,19 +15,18 @@ table_description_sql = %{
 }
 
 table_references_sql = %{
-  SELECT COLUMN_NAME,
-       --  CONSTRAINT_NAME,
-         REFERENCED_TABLE_NAME,
-         REFERENCED_COLUMN_NAME
+  SELECT TABLE_NAME,
+         COLUMN_NAME,
+         REFERENCED_TABLE_NAME
     FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
    WHERE REFERENCED_TABLE_SCHEMA = 'mydb'
-     AND REFERENCED_TABLE_NAME = '%{table_name}';
+     AND TABLE_NAME = '%{table_name}';
 }
 
-list_of_tables = `mysql --raw --silent -uroot mydb -e "#{table_list_sql}"`
+list_of_tables = `mysql --silent -uroot mydb -e "#{table_list_sql}"`
 
 result =
-  list_of_tables.split("\n").first(2).reduce({}) do |acc, table|
+  list_of_tables.split("\n").reduce({}) do |acc, table|
     sql = table_description_sql % {table_name: table}
     sql2 = table_references_sql % {table_name: table}
 
@@ -36,8 +35,12 @@ result =
       references: []
     }
 
-    result1 = `mysql --raw --silent -uroot mydb -e "#{sql}"`
-    result2 = `mysql --raw --silent -uroot mydb -e "#{sql2}"`
+    result1 = `mysql --silent -uroot mydb -e "#{sql}"`
+
+    puts result1.inspect
+    result2 = `mysql --silent -uroot mydb -e "#{sql2}"`
+
+    puts result2.inspect
 
     result1.split("\n").each do |line|
       key,
@@ -58,18 +61,57 @@ result =
     end
 
     result2.split("\n").each do |line|
-      key,
-      ref_table_name,
-      ref_col_name = line.split("\t")
+      table_name,
+      column_name,
+      ref_table_name = line.split("\t")
 
       acc[table][:references] << {
-        key:            key,
-        ref_table_name: ref_table_name,
-        ref_col_name:   ref_col_name
+        table_name:     table_name,
+        column_name:    column_name,
+        ref_table_name: ref_table_name
       }
     end
 
     acc
   end
 
+output = ""
+
+result.each do |tbl_name, tbl_info|
+  output << "[#{tbl_name}]\n"
+
+  tbl_info[:columns].each do |col|
+    nullable = col[:is_nullable] == "NO" ? "NOT NULL" : nil
+
+    output << [col[:name], col[:type], nullable].compact.join(" | ") << "\n"
+  end
+
+  if tbl_info[:references].size > 0
+    output << "\n"
+
+    tbl_info[:references].each do |ref|
+      this_col = tbl_info[:columns].find { |c| c[:name] == ref[:column_name] }
+
+      right =
+        if this_col[:is_nullable] == "NO"
+          "1"
+        else
+          "?"
+        end
+
+      left = "1"
+
+      output << "#{ref[:table_name]} #{left}--#{right} #{ref[:ref_table_name]}\n"
+    end
+  end
+
+  output << "\n"
+end
+
+File.open("/tmp/output.txt", "w+") do |file|
+  file.puts output
+end
+
 puts JSON.pretty_generate(result)
+
+`./simple-erd -i /tmp/output.txt -o /tmp/yo.png -f png`
