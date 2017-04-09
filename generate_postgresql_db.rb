efe23d3d0ba3e5ad993df6d2cac32d0f8d1eafe7
gmile <iamexile@gmail.com>
2017-04-09 14:00:31 +0200

    WIP primitive type conversion between DBs

--- generate_postgresql_db.rb
+++ generate_postgresql_db.rb
@@ -24,33 +24,102 @@ output_filename = required_options[:output]
 input = IO.read(input_filename)
 table_data = JSON.parse(input)
 
-table_template = %{CREATE TABLE %{table_name} (
+table_template = %{
+%{new_types}
+
+CREATE TABLE %{table_name} (
 %{rows}
 );
 }
 
 output = ""
 
+def translate_type_from_mysql_to_pgsql(name, type)
+  case
+  when type =~ /^int/           then "integer"
+  when type =~ /^datetime/      then "timestamp"
+  when type =~ /^varchar\(36\)/ then "uuid"
+  when type =~ /^enum/          then
+    type.match(/^enum\(.*\)/)[1]&.split(",")&.map(&:strip) || ["default1", "default2"]
+  else
+    type
+  end
+end
+
+def custom_fix(tbl_name, name, type)
+  pg_name, array =
+    if name.end_with?("[]")
+      [name.delete("[]"), true]
+    else
+      [name, false]
+    end
+
+  pg_type = translate_type_from_mysql_to_pgsql(name, type)
+
+  pg_type =
+    if pg_type.is_a? Array
+      type_name = "#{tbl_name}_#{name}"
+      puts "WARNING: need to register a new enum type \"#{type_name}\" with values: #{pg_type.join(", ")}"
+
+      [type_name, pg_type]
+    else
+      pg_type
+    end
+
+  pg_type =
+    if array
+      pg_type << "[]"
+    else
+      pg_type
+    end
+
+  [pg_name, pg_type]
+end
+
 output =
-  table_data.first(2).map do |tbl_name, columns|
+  table_data.first(3).map do |tbl_name, columns|
     max_name = columns.max_by { |c| c["col_name"] }["col_name"].size
     max_type = columns.max_by { |c| c["col_type"] }["col_type"].size
 
+    new_types = []
+
     cols_out =
       columns.map do |column|
-        "  %-#{max_name + 5}{name}%-#{max_type}{type}" % {
-          name: column["col_name"],
-          type: column["col_type"]
+        name, type = custom_fix(tbl_name, column["col_name"], column["col_type"])
+
+        t = if type.is_a? Array
+              new_types << type
+
+              type[0]
+            else
+              type
+            end
+
+        "  %-#{max_name + 10}{name}%-#{max_type}{type}" % {
+          name: name,
+          type: t
         }
       end
 
     table_template % {
+      new_types: "",
       table_name: tbl_name,
       rows: cols_out.join(",\n")
     }
   end.join("\n")
 
 puts output
+
+db_script = "/tmp/test_psql_db.sql"
+
+File.open(db_script, "w+") do |file|
+  file.puts output
+end
+
+`dropdb test`
+`createdb test`
+
+`psql test < #{db_script}`
 
 # CREATE TABLE films (
 #     code        char(5) CONSTRAINT firstkey PRIMARY KEY,
